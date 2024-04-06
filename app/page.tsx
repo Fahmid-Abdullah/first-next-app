0d6d98f9504b4b4fb1102c201b2986c07d6962b8
Fahmid-Abdullah <fahmidabdullah@hotmail.com>
2024-04-05 22:45:42 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { useState, FormEvent } from "react";
+import { useRouter } from 'next/navigation';
 
 export default function page() {
   const [inputVal, setInputVal] = useState("");
+  const { push } = useRouter();
 
   const handleSubmit = (event:FormEvent) => {
     event.preventDefault();
+    push(`/prediction/${inputVal}`);
   }
 
   return (
@@ -14,7 +17,7 @@ export default function page() {
         <h1>Enter Your Name</h1>
       </div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Type Your Name..." value={inputVal} onChange={(e) => setInputVal(e.target.value)}/>
+        <input type="text" className="text-black" placeholder="Type Your Name..." value={inputVal} onChange={(e) => setInputVal(e.target.value)}/>
         <button type="submit">Submit</button>
       </form>
     </div>
